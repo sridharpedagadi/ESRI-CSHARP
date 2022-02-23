befb56ed9ffbc06d7ab7b5b19d086e9e17074736
Sridhar Pedagadi <sp54931@corp.cyient.com>
2022-02-23 10:50:37 +0530

    test

---
+++ 202202-dotnet/Visual-Studio-projects/SimpleCSPrograms/Assignment4.1/Program.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Assignment4
+static void Main(string[] args)
+{
+           String title = "The ESRI Training";
+           String author = "Sridhar";
+           int price = 400;
+           double rating = 4;
+
+              Console.WriteLine("Title:\t" + title);
+
+              Console.WriteLine("Author:\t{0}", author);
+
+              Console.writeLine("Original Price: {0}\tRating: {1}", price, rating);
+
+            if (rating > 4 && price < 400)
+
+            {
+                Console.WriteLine("Recomendation = Highly recommned")
+                
+                if (rating < 3)
+
+                {
+                
+                Console.WriteLine("Recomendation = Not recommned")
+                    //if both condition 1 and condition2 is true 
+
+                }
+
+                else
+
+                {
+                
+                 Console.WriteLine("Recomendation = May recommned")
+                    //condition 1 is true but condition 2 is false 
+
+                }
+
+            }
+            else
+
+            {
+
+                //condition 1 is false and I don’t care about condition 2 
+
+            }
+
+        }
+    }
+
+}
---
+++ 202202-dotnet/Visual-Studio-projects/SimpleCSPrograms/HelloWorld2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Console.WriteLine("Hello\nWorld");
+            //System.Console.WriteLine("\"Hello\" from C#\r\"World\" of  ");
+            a = 5;
+            b = 5;
+
+            c = a++ * ++b;
+
+            Console.WriteLine("a={0}\tb={1}\tc={2}", a, b, c);
+
+        }
+    }
+}
